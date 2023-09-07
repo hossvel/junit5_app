62208f24f919b419d96a14833853d6e981ae39eb
devhoss <velasco2288@gmail.com>
2023-09-06 23:20:08 -0500

    @RepeatedTest repite numero de veces de la prueba

--- src/test/java/org/hossvel/CuentaTest.java
+++ src/test/java/org/hossvel/CuentaTest.java
@@ -258,8 +258,31 @@ class CuentaTest {
 }
 
 
+    @Nested
+    class repetirTest{
 
+    @DisplayName("Probando Debito Cuenta Repetir no parametro!")
+    @RepeatedTest(value=5, name = "{displayName} - Repetición numero {currentRepetition} de {totalRepetitions}")
+    void testDebitoCuentaRepetirNoParametro() {
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
 
+    @DisplayName("Probando Debito Cuenta Repetir!")
+    @RepeatedTest(value=5, name = "{displayName} - Repetición numero {currentRepetition} de {totalRepetitions}")
+    void testDebitoCuentaRepetir(RepetitionInfo info) {
+        if(info.getCurrentRepetition() == 3){
+            System.out.println("estamos en la repeticion " + info.getCurrentRepetition());
+        }
+        cuenta.debito(new BigDecimal(100));
+        assertNotNull(cuenta.getSaldo());
+        assertEquals(900, cuenta.getSaldo().intValue());
+        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+    }
+
+}
 
 
 }
